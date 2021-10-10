cc1f00c668cac5eb460fd0160b028dbc0d5b2341
Eugen Kakareka <evv.kakareka@gmail.com>
2021-10-10 16:29:08 +0300

    time list add

--- homework/src/components/Timer.jsx
+++ homework/src/components/Timer.jsx
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from "react";
 const Timer = () => {
   const [second, setSecond] = useState("00");
   const [minute, setMinute] = useState("00");
+  const [hour,setHour] = useState("00")
   const [counter, setCounter] = useState(0);
   const [isActive, setIsActive] = useState(false);
-  const [pause,setPause] = useState(false)
- 
+  const [pause, setPause] = useState(false);
+  const [timesList, setTimesList] = useState([]);
 
   useEffect(() => {
     let intervalId;
@@ -15,12 +16,15 @@ const Timer = () => {
       intervalId = setInterval(() => {
         const secondCounter = counter % 60;
         const minuteCounter = Math.floor(counter / 60);
+        const hourCounter = Math.floor(counter/ 3600)
 
-        const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}`: secondCounter;
-        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}`: minuteCounter;
+        const computedSecond = String(secondCounter).length === 1? `0${secondCounter}`: secondCounter;
+        const computedMinute = String(minuteCounter).length === 1? `0${minuteCounter}`: minuteCounter;
+        const computedHour = String(hourCounter).length ===1 ? `0${hourCounter}`: hourCounter;
 
         setSecond(computedSecond);
         setMinute(computedMinute);
+        setHour(computedHour);
 
         setCounter((counter) => counter + 1);
       }, 1000);
@@ -29,30 +33,47 @@ const Timer = () => {
     return () => clearInterval(intervalId);
   }, [isActive, counter]);
 
-  function stopTimer() {
+  function resetTimer() {
     setIsActive(false);
     setPause(false);
     setCounter(0);
     setSecond("00");
     setMinute("00");
+    setHour("00");
+    setTimesList([...timesList,`${hour}:${minute}:${second}`])
+  }
+
+  function stopTimer(){
+    setIsActive(false);
+    setPause(true);
+    setTimesList([...timesList,`${hour}:${minute}:${second}`])
   }
 
   
-
+    
   return (
     <div className="container">
-      <div className="time">
-        <span className="minute">{minute}</span>
-        <span>:</span>
-        <span className="second">{second}</span>
+      <div className="containerTime">
+        <div className="time">
+          <span className="hour">{hour}</span>
+          <span>:</span>
+          <span className="minute">{minute}</span>
+          <span>:</span>
+          <span className="second">{second}</span>
+        </div>
+        <div className="buttons">
+          {pause ? (<button onClick={() => setIsActive(true)} className="continue">Continue</button>) 
+          : (<button onClick={() => setIsActive(true)} className="start">Start</button>)
+          }
+          <button onClick={stopTimer} className="stop">Stop</button>
+          <button onClick={resetTimer} className="reset">Reset</button>
+        </div>
       </div>
-      <div className="buttons">
-        {pause 
-        ? <button onClick={() => setIsActive(true)} className="continue">Continue</button> 
-        : <button onClick={() => setIsActive(true)} className="start">Start</button>
-        }
-        <button onClick={() =>{setIsActive(false);setPause(true)}}  className="stop">Stop</button>
-        <button onClick={stopTimer} className="reset">Reset</button>
+
+      <div className="containerList">
+        <ol>
+          {timesList.map((time)=>(<li>{time}</li>))}
+        </ol>
       </div>
     </div>
   );
